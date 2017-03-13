5c87ceb9ac221937a657f53ac522460a244d5dc3
Till Biskup <till@till-biskup.de>
2017-03-13 10:47:36 +0100

    Updated example to work with new version of toolbox

--- Old/examples/varyD_E0.m
+++ Old/examples/varyD_E0.m
@@ -10,23 +10,28 @@
 % ways. This script may serve as a starting point for similar systematic
 % investigations of the parameters.
 
-% Copyright (c) 2013, Till Biskup, <till@till-biskup.de>
-% 2013-10-18
+% Copyright (c) 2013-17, Till Biskup, <till@till-biskup.de>
+% 2017-03-13
 
 % Prepare dataset for simulation
 dataset = TsimDataset();
-dataset = TsimSimini(dataset);
+dataset = TsimIniSimpar(dataset);
 
 % Change a few parameters in Sys and Exp structure
 % isotropic g tensor
-dataset.TSim.sim.Sys.g = [2 2 2];
+dataset.Tsim.sim.simpar.Sys.gx = 2;
+dataset.Tsim.sim.simpar.Sys.gy = 2;
+dataset.Tsim.sim.simpar.Sys.gz = 2;
 % Field range and stepping
-dataset.TSim.sim.Exp.Range = [0 550]; % in mT
-dataset.TSim.sim.Exp.nPoints = 551;
+dataset.Tsim.sim.simpar.Range = [0 550]; % in mT
+dataset.Tsim.sim.simpar.nPoints = 551;
 % Population
-dataset.TSim.sim.Exp.Temperature = [0.5 0.5 0];
+dataset.Tsim.sim.simpar.p1 = 0.5;
+dataset.Tsim.sim.simpar.p2 = 0.5;
+dataset.Tsim.sim.simpar.p3 = 0;
 % isotropic line width
-dataset.TSim.sim.Sys.lw = 0; % in mT
+dataset.TsSim.sim.simpar.lwGauss = 0; % in mT
+dataset.TsSim.sim.simpar.lwLorentz = 0; % in mT
 
 % Get range for D
 D = 1000 : 100 : 1400; % in MHz
@@ -36,7 +41,7 @@ E = 0;
 
 % Preallocate cell array for speed
 simdataset = cell(length(D),1);
-data = zeros(dataset.TSim.sim.Exp.nPoints,length(D));
+data = zeros(dataset.Tsim.sim.simpar.nPoints,length(D));
 
 % Be kind and tell the user what is going on
 fprintf('Calculating %i spectra\n',length(D));
@@ -45,7 +50,10 @@ fprintf('Calculating %i spectra\n',length(D));
 for k = 1:length(D)
    
     % Set D tensor respectively
-    dataset.TSim.sim.Sys.D = [-D(k)/3+E -D(k)/3-E 2*D(k)/3];
+    dataset.Tsim.sim.simpar.D = D(k);
+    dataset.Tsim.sim.simpar.E = E;
+    % Set parameters accordingly
+    dataset = TsimSimpar2EasySpin(dataset);
     % Do actual simulation
     simdataset{k} = TsimSim(dataset);
     % Create 2D matrix with data, B0 vs. D
@@ -66,7 +74,7 @@ fprintf('\n');
 
 % Plot D as function of B0
 figure();
-imagesc(D,dataset.TSim.sim.Exp.Range,data);
+imagesc(D,dataset.Tsim.sim.simpar.Range,data);
 set(gca,'YDir','normal');
 set(gca,'XLim',[min(D) max(D)]);
 xlabel('{\it D} / MHz');
@@ -76,9 +84,9 @@ title('Triplet spectra as function of the ZFS parameter {\it D}');
 % Plot first and last spectrum on top of each other
 figure();
 xaxis = linspace(...
-    min(dataset.TSim.sim.Exp.Range),...
-    max(dataset.TSim.sim.Exp.Range),...
-    dataset.TSim.sim.Exp.nPoints ...
+    min(dataset.Tsim.sim.simpar.Range),...
+    max(dataset.Tsim.sim.simpar.Range),...
+    dataset.Tsim.sim.simpar.nPoints ...
     );
 plot(...
     xaxis,data(:,1),'k-',...
@@ -88,7 +96,7 @@ legend(...
     sprintf('{\\it D} = %i MHz',D(1)),...
     sprintf('{\\it D} = %i MHz',D(end))...
     );
-set(gca,'XLim',dataset.TSim.sim.Exp.Range);
+set(gca,'XLim',dataset.Tsim.sim.simpar.Range);
 xlabel('{\it magnetic field} / mT');
 ylabel('{\it intensity} / a.u.');
 title('Triplet spectra as function of the ZFS parameter {\it D}');
